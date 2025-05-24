e5d18af5badb019db86ece5e4b5265712102bcf7
h4sanzade <h4sanzade@gmail.com>
2025-05-24 16:42:36 +0400

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,12 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+
+    androidTestImplementation("androidx.navigation:navigation-testing:2.7.7")
+
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
 
 
     implementation(libs.androidx.core.ktx)
