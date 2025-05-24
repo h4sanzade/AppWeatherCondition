80af01a8cfadd59aa0166129a485933c2dab2c7d
h4sanzade <h4sanzade@gmail.com>
2025-05-24 16:15:04 +0400

    Initial commit

---
+++ app/src/main/java/com/materialdesign/weatherapp/RetrofitClient.kt
@@ -0,0 +1,15 @@
+package com.materialdesign.weatherapp
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "https://api.weatherapi.com/v1/"
+
+    val instance: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
---
+++ app/src/main/java/com/materialdesign/weatherapp/WeatherApiService.kt
@@ -0,0 +1,12 @@
+package com.materialdesign.weatherapp
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherApiService {
+    @GET("current.json")
+    suspend fun getCurrentWeather(
+        @Query("key") apiKey: String,
+        @Query("q") location: String
+    ): WeatherResponse
+}
